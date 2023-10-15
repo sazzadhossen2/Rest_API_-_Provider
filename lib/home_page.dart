61ae0e3e7d841628769d94cec85d66c1cb5a6445
Sazzad Hossen <89637675+sazzadhossen2@users.noreply.github.com>
2023-10-15 10:46:58 +0600

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,12 @@
-
+//
 // import 'dart:convert';
 //
 // import 'package:flutter/cupertino.dart';
 // import 'package:flutter/material.dart';
 // import 'package:http/http.dart' as http;
-// import 'package:livetest/model/postmodel.dart';
+//
+// import 'Model/postmodel.dart';
+//
 //
 //
 // class Homepage extends StatefulWidget {
@@ -15,45 +17,70 @@
 // }
 //
 // class _HomepageState extends State<Homepage> {
-//   List<Postmodel> postlist=[];
-//   Future<List<Postmodel>> getPostApi()async{
-//     final response =await http.get(Uri.parse("https://jsonplaceholder.typicode.com/posts"));
-//     var data = jsonDecode(response.body.toString());
-//     if(response.statusCode ==200){
-//       for(Map <String, dynamic>i in data){
-//         postlist.add(Postmodel.fromJson(i));
-//       }
-//       return postlist;
+// List<Postmodel>postlist=[];
+//   Future<List<Postmodel>>getputApi()async{
+//     final responc = await http.get(Uri.parse("https://jsonplaceholder.typicode.com/posts"));
+//     var data = jsonDecode(responc.body.toString());
+//   if(responc.statusCode==200){
+//     postlist.clear();
+//     for(Map<String,dynamic>i in data){
+//       postlist.add(Postmodel.fromJson(i));
 //     }
-//     else{
-//       return postlist;
-//     }
+//     return postlist;
+//   }
+//   return postlist;
 //   }
 //
 //   @override
 //   Widget build(BuildContext context) {
 //     return Scaffold(
-//       appBar: AppBar(title: Text("Mu Apps"),),
-//       body: Column(
+//       appBar: AppBar(title: Text("My Apps"),),
+//       body:Column(
 //         children: [
-//           Expanded(
-//             child: FutureBuilder(future: getPostApi(),
-//                 builder:(context,snapshot){
-//               if(!snapshot.hasData){
-//                 return Text("Loading");
-//               }
-//               else {
-//                 return ListView.builder(
-//                     itemCount: postlist.length,
-//                     itemBuilder: (context,index){
-//                   return Text(index.toString());
-//                 });
-//               }
+// Expanded(
+//   child:   FutureBuilder(future: getputApi(),
 //
-//             }),
-//           )
+//       builder: (context,snapshot){
+//
+//     if(!snapshot.hasData){
+//
+//       return Text("Loading");
+//
+//     }
+//
+//     else{
+//
+//       return ListView.builder(
+//
+//           itemCount: postlist.length,
+//
+//           itemBuilder: (context,index){
+//
+//         return Card(
+//           child: Padding(
+//             padding: const EdgeInsets.all(18.0),
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.start,
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 Text("Title",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+//                 Text(postlist[index].body.toString()),
+//                SizedBox(height: 5,),
+//                Text("Discription",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+//                Text(postlist[index].body.toString())
+//               ],
+//             ),
+//           ),
+//         );
+//
+//       });
+//
+//     }
+//
+//       }),
+// )
 //         ],
-//       ),
+//       )
 //     );
 //   }
 // }
